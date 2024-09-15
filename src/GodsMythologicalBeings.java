c2b8812388b0b897dc9a4803e81e135bfdb50896
sebas1705sanchez <s.sanchez8@utp.edu.co>
2024-09-15 17:50:43 -0500

    clase GodsMythologicalBeings con atributos y metodos

--- src/GodsMythologicalBeings.java
+++ src/GodsMythologicalBeings.java
@@ -1,3 +1,22 @@
 public class GodsMythologicalBeings {
-    String habilidad;
+    private Personaje character;
+    private String specialAbility;
+
+    public GodsMythologicalBeings(Personaje character, String specialAbility) {
+        this.character = character;
+        this.specialAbility = specialAbility;
+    }
+
+    public String getName() {
+        return character.getName();
+    }
+
+    public String getSpecialAbility() {
+        return specialAbility;
+    }
+
+    public void setSpecialAbility(String specialAbility) {
+        this.specialAbility = specialAbility;
+    }
+
 }
