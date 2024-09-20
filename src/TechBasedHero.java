2a46243eaca4d6a4d30b0f868167e19f2a477663
JuandrezG <julianrosso10@gmail.com>
2024-09-19 20:00:43 -0500

    Character techBasedHero make it

--- src/TechBasedHero.java
+++ src/TechBasedHero.java
@@ -1,7 +1,30 @@
 public class TechBasedHero {
-    String habilidad;
-    int techLevel;
+    private int techLevel; //nivel de tecnologia
+    private String techWeapon; //tecnologia del arma
+    private Personaje character; //es un objeto que trae los atributos de la clase personaje
 
+    public TechBasedHero(int techLevel, String techWeapon, Personaje character){
+        this.techLevel = techLevel;
+        this.techWeapon = techWeapon;
+        this.character = character;
+    }
+    public int getTechLevel(){
+        return techLevel;
+    }
+    public void setTechLevel(int techLevel){
+        this.techLevel = techLevel;
+    }
+    public String getTechWeapon(){
+        return techWeapon;
+    }
+    public void setTechWeapon(String techWeapon){
+        this.techWeapon = techWeapon;
+    }
+    public String getCharacterName(){
+        return character.getName();
+    }
+    public void setCharacterName(String name){
+        this.character.setName(name);
+    }
 
-    sasasa
 }
